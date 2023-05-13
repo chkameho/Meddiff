6bd57f3177c791197db045849e9d27ab0c28c107
chkameho <131600536+chkameho@users.noreply.github.com>
2023-05-13 16:52:31 +0200

    Update Meddiff.py

--- Meddiff.py
+++ Meddiff.py
@@ -42,6 +42,7 @@ def del_erste_Zählung():
 def Tastatur_Blutbild_Differenzierung(): 
     # Generiert ein Tastatur für die Blutbiddifferenzierung 
     #st.session_state wird gebraucht,damit die Zählung gelingt.
+    @st.cache
        
     if 'Basophilen' not in st.session_state:
         st.session_state.Basophilen=0
