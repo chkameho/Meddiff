d4de05d9413b5c431d18381b57e84c53a0c19c1b
chkameho <131600536+chkameho@users.noreply.github.com>
2023-05-14 22:49:33 +0200

    Update 1_📈_Archiv.py

--- pages/1_📈_Archiv.py
+++ pages/1_📈_Archiv.py
@@ -59,8 +59,9 @@ zugeschnittene_Patienten_Daten = gewählte_Patienten_Daten_gedreht.iloc[:16]
 zugeschnittene_Patienten_Daten.columns = ["Einheit: %"]
 if Leukozyten_Wert != 0:
       zugeschnittene_Patienten_Daten["Einheit: G/L"]= (Leukozyten_Wert / 100.00) * zugeschnittene_Patienten_Daten["Einheit: %"]
-st.write(zugeschnittene_Patienten_Daten)
 zugeschnittene_Patienten_Daten.rename(columns={zugeschnittene_Patienten_Daten.columns[0]: 'Parameter'}, inplace=True)
+st.write(zugeschnittene_Patienten_Daten)
+
 
 # Extrahiere den Namen
 name = zugeschnittene_Patienten_Daten.index[]
