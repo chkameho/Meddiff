5743080a97058be141914a7a83b7022c18020dea
chkameho <131600536+chkameho@users.noreply.github.com>
2023-05-14 21:08:39 +0200

    Add files via upload

---
+++ pages/1_📈_Archiv.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import yaml
+from yaml.loader import SafeLoader
+from jsonbin import load_key
+import streamlit_authenticator as stauth
+
+
+#####################secrets#####################################################
+#Jsonbin_1
+jsonbin_secrets_1 = st.secrets["jsonbin_1"]
+api_key_1 = jsonbin_secrets_1["api_key"]
+bin_id_1 = jsonbin_secrets_1["bin_id"]
+
+#Jsonbin_2
+jsonbin_secrets_2 = st.secrets["jsonbin_2"]
+api_key_2 = jsonbin_secrets_2["api_key"]
+bin_id_2 = jsonbin_secrets_2["bin_id"]
+#####user login#################################################################
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+)
+
+fullname, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status == True:   # login successful
+    authenticator.logout('Logout', 'main')   # show logout button
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+    st.stop()
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+    st.stop()
+###############################################################################
+#Funktionen 
+
+def load_data():
+    load =load_key(api_key_1, bin_id_1, username)
+    if load == None:
+        load=[]
+    return load
+
+st.title("Datein")
+Daten = load_key
