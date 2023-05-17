7f33c01a6419d16accf551440568ca59c1563a76
chkameho <131600536+chkameho@users.noreply.github.com>
2023-05-17 20:58:02 +0200

    Update 1_📈_Archiv.py

--- pages/1_📈_Archiv.py
+++ pages/1_📈_Archiv.py
@@ -65,36 +65,36 @@ else:
     Identifikationsnummer=Identifikation_sortieren(Identifikationsnummer)
     Patienten_Identifikation_Auswahl=st.selectbox("Selektiere die Identifikationsnummer",(Identifikationsnummer))
     gewählte_Patienten_Daten=DataFrame[DataFrame["Identifikationsnummer"]== Patienten_Identifikation_Auswahl]
-if len(gewählte_Patienten_Daten)>1:
-    #Speicherzeit später korregieren
-    Speicherzeit=gewählte_Patienten_Daten["Specherzeit"]
-    Nach_Speicherzeit_selektieren=st.selectbox("Selektiere die Identifikationsnummer",(Speicherzeit))
-    gewählte_Patienten_Daten=DataFrame[DataFrame["Specherzeit"]== Nach_Speicherzeit_selektieren]
-Leukozyten_Wert = st.number_input("Leukozyten mit dem Einheit G/L")
-gewählte_Patienten_Daten_gedreht= gewählte_Patienten_Daten.T
-zugeschnittene_Patienten_Daten = gewählte_Patienten_Daten_gedreht.iloc[:16]
-zugeschnittene_Patienten_Daten.columns = ["Einheit: %"]
-if Leukozyten_Wert != 0:
-      zugeschnittene_Patienten_Daten["Einheit: G/L"]= (Leukozyten_Wert / 100.00) * zugeschnittene_Patienten_Daten["Einheit: %"]
-st.table(zugeschnittene_Patienten_Daten)
-if len(gewählte_Patienten_Daten["Legende"]) != 0:
-    st.write(gewählte_Patienten_Daten["Legende"].T)
+    if len(gewählte_Patienten_Daten)>1:
+        #Speicherzeit später korregieren
+        Speicherzeit=gewählte_Patienten_Daten["Specherzeit"]
+        Nach_Speicherzeit_selektieren=st.selectbox("Selektiere die Identifikationsnummer",(Speicherzeit))
+        gewählte_Patienten_Daten=DataFrame[DataFrame["Specherzeit"]== Nach_Speicherzeit_selektieren]
+    Leukozyten_Wert = st.number_input("Leukozyten mit dem Einheit G/L")
+    gewählte_Patienten_Daten_gedreht= gewählte_Patienten_Daten.T
+    zugeschnittene_Patienten_Daten = gewählte_Patienten_Daten_gedreht.iloc[:16]
+    zugeschnittene_Patienten_Daten.columns = ["Einheit: %"]
+    if Leukozyten_Wert != 0:
+         zugeschnittene_Patienten_Daten["Einheit: G/L"]= (Leukozyten_Wert / 100.00) * zugeschnittene_Patienten_Daten["Einheit: %"]
+    st.table(zugeschnittene_Patienten_Daten)
+    if len(gewählte_Patienten_Daten["Legende"]) != 0:
+        st.write(gewählte_Patienten_Daten["Legende"].T)
 
-# Extrahiere den Namen
-name = zugeschnittene_Patienten_Daten.index
+    # Extrahiere den Namen
+    name = zugeschnittene_Patienten_Daten.index
 
-# Erstelle ein Pie-Chart
-fig = px.pie(zugeschnittene_Patienten_Daten, values='Einheit: %', names=name, title=f"Leukozytenverteilung")
+    # Erstelle ein Pie-Chart
+    fig = px.pie(zugeschnittene_Patienten_Daten, values='Einheit: %', names=name, title=f"Leukozytenverteilung")
 
-# Zeige das Pie-Chart in Streamlit
-st.plotly_chart(fig)
+    # Zeige das Pie-Chart in Streamlit
+    st.plotly_chart(fig)
 
-Bewertungen = gewählte_Patienten_Daten_gedreht.iloc[18:21]
-st.table(Bewertungen)
+    Bewertungen = gewählte_Patienten_Daten_gedreht.iloc[18:21]
+    st.table(Bewertungen)
 
-# Add a download button
-csv = gewählte_Patienten_Daten.to_csv(index=False) # Convert the DataFrame to CSV
-b64 = base64.b64encode(csv.encode()).decode() # Encode to base64
-href = f'<a href="data:file/csv;base64,{b64}" download="my_file.csv">Download CSV file</a>'
-st.markdown(href, unsafe_allow_html=True)
+    # Add a download button
+    csv = gewählte_Patienten_Daten.to_csv(index=False) # Convert the DataFrame to CSV
+    b64 = base64.b64encode(csv.encode()).decode() # Encode to base64
+    href = f'<a href="data:file/csv;base64,{b64}" download="my_file.csv">Download CSV file</a>'
+    st.markdown(href, unsafe_allow_html=True)
 
