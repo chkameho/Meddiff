e2b1d0f501d1bd99ef1a3ab7fb1bc08f84470fd1
chkameho <131600536+chkameho@users.noreply.github.com>
2023-05-17 15:08:28 +0200

    Update Meddiff.py

--- Meddiff.py
+++ Meddiff.py
@@ -400,7 +400,7 @@ with tab1:
             clear_session_state()
             session_state_initialisieren()
 
-    Aktuelle_Zählung=pd.DataFrame(Zählung_Dictionary(),index=["Aktuelle Zählung"])
+    Aktuelle_Zählung=pd.DataFrame(Zählung_Dictionary(),index=["Aktuelle Zählung"]).T
     st.table(Aktuelle_Zählung)
         
 ##################################################################################
