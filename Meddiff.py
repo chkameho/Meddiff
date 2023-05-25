1b75488be506fc9230e37c6ade1c7ee47dfb7543
chkameho <131600536+chkameho@users.noreply.github.com>
2023-05-25 19:25:48 +0200

    Update Meddiff.py

--- Meddiff.py
+++ Meddiff.py
@@ -543,7 +543,7 @@ with tab3:
             # Get the predicted class from the response
             result = json.loads(response.content.decode())
             result = pd.DataFrame(result)
-            st.write(result)
+            st.write([result])
 
             st.image(image_file)
 
