7b2d8803921cbaca1b847a04f7598816be9b671d
chkameho <131600536+chkameho@users.noreply.github.com>
2023-05-29 17:48:13 +0200

    Update Meddiff.py

--- Meddiff.py
+++ Meddiff.py
@@ -541,6 +541,7 @@ with tab3:
             response = requests.post(API_URL, headers=headers, data=image_bytes)
             # Get the predicted class from the response
             result = json.loads(response.content.decode())
+            st.write(restult)
             result = pd.DataFrame(result)
             st.write(result)
             st.image(image_file)
