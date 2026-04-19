b37b3c163c41aea6d5d9ac75a1f561eaec1943ca
chkameho <kamen1995@hotmail.com>
2026-04-19 22:35:30 +0200

    Add two new functions to control the flow of st.session_state.

---
+++ utils/manipulate_session_state.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+def transfer_category_values(count_time_1, count_time_2, category):
+    """
+    Copies all values of a given category from one session_state entry to another.
+
+    Args:
+        count_time_1 (str): Key of the target session_state entry (the one being updated).
+        count_time_2 (str): Key of the source session_state entry (the one providing the values).
+        category (str): The category whose values should be transferred.
+    """
+    for key in st.session_state[count_time_1][category]:
+        st.session_state[count_time_1][category] = st.session_state[count_time_2][category]
+
+
+def transfer_count(count_time_1,count_time_2):
+    """
+    Copies predefined count categories from one session_state entry to another.
+
+    Args:
+        count_time_1 (str): Key of the target session_state entry (the one being updated).
+        count_time_2 (str): Key of the source session_state entry (the one providing the values).
+    """
+    transfer_category_values(count_time_1, count_time_2, "leucocyte_count")
+    transfer_category_values(count_time_1, count_time_2, "diverse_count")
+
+    
