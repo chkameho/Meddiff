51cd697abca37a3c5ecab8162cb46af0646e3ffe
chkameho <kamen1995@hotmail.com>
2026-04-28 17:57:32 +0200

    change the name of all functions in this file

--- utils/manipulate_session_state.py
+++ utils/manipulate_session_state.py
@@ -2,7 +2,7 @@ import streamlit as st
 import copy
 
 
-def transfer_category_values(count_time_1, count_time_2, category):
+def copy_session_state_category(count_time_1, count_time_2, category):
     """
     Copies all values of a given category from one session_state entry to another.
 
@@ -15,7 +15,7 @@ def transfer_category_values(count_time_1, count_time_2, category):
         st.session_state[count_time_2][category])
 
 
-def transfer_count(count_time_1,count_time_2):
+def copy_default_count_categories(count_time_1,count_time_2):
     """
     Copies predefined count categories from one session_state entry to another.
 
@@ -23,7 +23,7 @@ def transfer_count(count_time_1,count_time_2):
         count_time_1 (str): Key of the target session_state entry (the one being updated).
         count_time_2 (str): Key of the source session_state entry (the one providing the values).
     """
-    transfer_category_values(count_time_1, count_time_2, "leucocyte_count")
-    transfer_category_values(count_time_1, count_time_2, "diverse_count")
+    copy_session_state_category(count_time_1, count_time_2, "leucocyte_count")
+    copy_session_state_category(count_time_1, count_time_2, "diverse_count")
 
     
